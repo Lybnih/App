330a72839c22b3273b4e610b1482e1a14cf2613f
Lybnih <132515156+Lybnih@users.noreply.github.com>
2024-08-14 21:12:27 -0300

    Add files via upload

--- src/mocks/sobre_nos.js
+++ src/mocks/sobre_nos.js
@@ -1,5 +1,6 @@
 import Logo from "../../assets/icon.png";
 import Local from "../../assets/sobre/local.jpeg"
+import Video from "../../assets/sobre/livraria_video.mp4"
 
 const sobre_nos = {
     
@@ -8,6 +9,7 @@ const sobre_nos = {
         imagem: Logo,
         txt: "A Great Book é uma plataforma dedicada a proporcionar acesso diversificado a obras literárias, incentivando a aprendizagem e a descoberta por meio de uma vasta biblioteca digital. Com uma equipe comprometida, buscamos constantemente inovar para oferecer experiências de leitura excepcionais.",
         img_local: Local,
+        video: Video,
 }
 }
 export default sobre_nos;
