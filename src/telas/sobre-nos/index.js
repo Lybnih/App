ef9841483f323534759f429cd44fb213c267dfeb
Lybnih <132515156+Lybnih@users.noreply.github.com>
2024-08-14 21:12:50 -0300

    Add files via upload

--- src/telas/sobre-nos/index.js
+++ src/telas/sobre-nos/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Image, View, ScrollView, ImageBackground } from 'react-native';
+import { Video, ResizeMode } from 'expo-av';
 import Texto from '../../componentes/Texto.js';
 import styles from './estilo.js';
 import Mock from '../../mocks/sobre_nos.js';
 
 export default function Sobre() {
-    return (
+    const video = React.useRef(null);
+    const [status, setStatus] = React.useState({});
 
+    return (
 
         <View style={styles.tela}>
             <Texto style={styles.titulo}>{Mock.topo.titulo}</Texto>
@@ -17,11 +20,25 @@ export default function Sobre() {
                             <Image source={Mock.topo.imagem} style={styles.logo} resizeMode='contain' />
                             <Texto style={styles.texto}>{Mock.topo.txt}</Texto>
                             <Image source={Mock.topo.img_local} style={styles.imagem} resizeMode='contain' />
+                            
+                            <Video
+                                ref={video}
+                                style={styles.video}
+                                source={{
+                                    uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+                                }}
+                                useNativeControls
+                                resizeMode={ResizeMode.CONTAIN}
+                                isLooping
+                                onPlaybackStatusUpdate={status => setStatus(() => status)}
+                            />
+
+                            </View>
                         </View>
-                    </View>
                 </ScrollView>
             </ImageBackground>
         </View>
 
     );
 }
+
